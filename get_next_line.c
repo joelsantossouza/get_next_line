46e6d3781d6ce8ef64ac26a2d3b0dd61ad3ce726
joel <joelsantossouza08@gmail.com>
2025-10-29 09:02:39 +0000

    Put all functions needed in utils

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,53 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 08:45:01 by joesanto          #+#    #+#             */
-/*   Updated: 2025/10/29 08:45:03 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/10/29 09:01:24 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+#include <unistd.h>
 
+static
+char	end_of_file(int fd, char buffer[BUFFER_SIZE], char **ptr, char **line)
+{
+	ssize_t	bytes_read;
+
+	if (!**ptr)
+	{
+		*ptr = buffer;
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read < 0)
+		{
+			*line = ft_realloc(*line, 0, 0);
+			buffer[0] = 0;
+		}
+		else
+			buffer[bytes_read] = 0;
+	}
+	return (**ptr == 0);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	buffer[BUFFER_SIZE + 1];
+	static char	*ptr = buffer;
+	const char	*start;
+	char		*line;
+	size_t		len;
+
+	len = 0;
+	line = 0;
+	while (!end_of_file(fd, buffer, &ptr, &line))
+	{
+		start = ptr;
+		ptr = ft_strchrnul(start, '\n');
+		ptr += *ptr == '\n';
+		line = ft_realloc(line, len, len + (ptr - start) + 1);
+		ft_strlcpy(line + len, start, (ptr - start) + 1);
+		len += ptr - start;
+		if (*(ptr - 1) == '\n')
+			break ;
+	}
+	return (line);
+}
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,73 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 08:45:41 by joesanto          #+#    #+#             */
-/*   Updated: 2025/10/29 08:45:42 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/10/29 08:53:56 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 
+static size_t	ft_min(size_t n1, size_t n2)
+{
+	if (n1 < n2)
+		return (n1);
+	return (n2);
+}
+
+static void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char		*pdest;
+	const unsigned char	*psrc;
+
+	if (!dest || !src)
+		return (0);
+	pdest = (unsigned char *) dest;
+	psrc = (const unsigned char *) src;
+	while (n--)
+		*pdest++ = *psrc++;
+	return (dest);
+}
+
+void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
+{
+	void	*new;
+
+	new = 0;
+	if (new_size)
+		new = ft_memcpy(malloc(new_size), ptr, ft_min(old_size, new_size));
+	free(ptr);
+	return (new);
+}
+
+char	*ft_strchrnul(const char *s, int c)
+{
+	char	find;
+
+	find = (char) c;
+	if (s)
+		while (*s)
+			if (*s++ == find)
+				return ((char *)(s - 1));
+	return ((char *) s);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	unsigned char		*pdst;
+	const unsigned char	*psrc;
+
+	psrc = (const unsigned char *) src;
+	if (src)
+	{
+		if (dst && size)
+		{
+			pdst = (unsigned char *) dst;
+			while (size-- > 1 && *psrc)
+				*pdst++ = *psrc++;
+			*pdst = 0;
+		}
+		while (*psrc)
+			psrc++;
+	}
+	return ((const char *) psrc - src);
+}
